0b28b7d23a60d3482a698c744c3f4e63f603f2fe
Mar-cere <marcelo0.nicolas@gmail.com>
2025-03-12 17:34:08 -0300

    chatScreen changes

--- src/services/openai.js
+++ src/services/openai.js
@@ -1,3 +1,6 @@
-export const OPENAI_API_KEY= 'sk-svcacct-X9UGt5fjoW-UZqorv9fSWv5SVZ4Ir3CZ9nfGEe10anL9v31d7VwOWDZRhg3sMT3BlbkFJV3xl_5BkYFliNdXk_5cVayWMaUhblQUijYVxjzGzMp0OonHVpw1vhzi6z7xAA';
+import { OPENAI_API_KEY as API_KEY } from '@env';
+
+// Configuración para la API de OpenAI
+export const OPENAI_API_KEY = API_KEY || '';
 export const OPENAI_API_URL = 'https://api.openai.com/v1/chat/completions';
-export const OPENAI_MODEL = 'gpt-3.5-turbo'; // Puedes usar 'gpt-4' si tienes acceso
+export const OPENAI_MODEL = 'gpt-3.5-turbo';
