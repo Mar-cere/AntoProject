824b82eb879101dd7a98f382fe252b625e123cee
Mar-cere <marcelo0.nicolas@gmail.com>
2025-03-27 21:10:37 -0300

    uri fixed

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use('/api/tasks', taskRoutes);
 
 // Establecer la conexión antes de importar modelos
-await mongoose.connect(process.env.MONGODB_URI, {
+await mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
