e38a549e2361d9b7077482d6344ffe9d0bfd099f
Mar-cere <marcelo0.nicolas@gmail.com>
2025-04-16 19:51:10 -0400

    protect

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { protect } from '../middleware/authMiddleware.js';
+import { authenticateToken as protect } from '../middleware/auth.js';
 import Message from '../models/Message.js';
 
 const router = express.Router();
